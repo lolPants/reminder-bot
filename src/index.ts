cb2dcee4d3b78e18866fd5b822b9368373472f9b
Jack Baron <jackmbaron@gmail.com>
2021-08-18 17:16:39 +0100

    setup discord client

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
 import 'source-map-support/register.js'
+
+import { Client, Intents } from 'discord.js'
+import { TOKEN } from './env/index.js'
+import { exitHook } from './exit.js'
+
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MEMBERS,
+  ],
+})
+
+exitHook(async exit => {
+  client.destroy()
+  exit()
+})
+
+void client.login(TOKEN)
