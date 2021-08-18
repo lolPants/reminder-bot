d7ffe0e3b8d1e48ce2839289f9f628a677862610
Jack Baron <jackmbaron@gmail.com>
2021-08-18 17:04:17 +0100

    setup env helpers

---
+++ src/env/index.ts
@@ -0,0 +1,11 @@
+import { registerString } from './register.js'
+
+// #region Globals
+const NODE_ENV = registerString('NODE_ENV')
+const IS_PROD = NODE_ENV?.toLowerCase() === 'production'
+export const IS_DEV = !IS_PROD
+// #endregion
+
+// #region Bot
+export const TOKEN = registerString('TOKEN', true)
+// #endregion
