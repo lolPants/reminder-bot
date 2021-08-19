0a0f4e472380e1142f738bc0a84c1722259a7ea3
Jack Baron <jackmbaron@gmail.com>
2021-08-19 15:26:53 +0100

    set reminder and fail on closed DMs

--- src/commands/remindme.ts
+++ src/commands/remindme.ts
@@ -1,4 +1,6 @@
 import { parse } from '@lolpants/timeparser'
+import { DiscordAPIError } from 'discord.js'
+import { addReminder, removeReminder } from '../reminders.js'
 import type { CommandHandler } from './index.js'
 
 const units = new Set([
@@ -43,6 +45,23 @@ export const remindme: CommandHandler = async ({
     return
   }
 
-  // TODO
-  console.log({ time, content })
+  const triggerAt = new Date(Date.now() + time)
+  const id = await addReminder(message, content, triggerAt)
+
+  try {
+    // TODO
+    await message.author.send({ content: `reminder ID: \`${id}\`` })
+  } catch (error: unknown) {
+    if (error instanceof DiscordAPIError && error.code === 50_007) {
+      await removeReminder(id)
+      await message.reply(
+        'Unable to record reminder as your privacy settings prevent me from sending direct messages to you.\n' +
+          "Please keep them open else I won't be able to send the reminder either!"
+      )
+
+      return
+    }
+
+    throw error
+  }
 }
