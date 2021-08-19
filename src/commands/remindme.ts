b98692280eb4645af9d5d9ff174b495819342faf
Jack Baron <jackmbaron@gmail.com>
2021-08-19 14:53:23 +0100

    resolve args

--- src/commands/remindme.ts
+++ src/commands/remindme.ts
@@ -1,5 +1,33 @@
 import type { CommandHandler } from './index.js'
 
-export const remindme: CommandHandler = async () => {
+const units = new Set([
+  'second',
+  'minute',
+  'hour',
+  'day',
+  'week',
+  'month',
+  'year',
+])
+
+export const remindme: CommandHandler = async ({ content: messageContent }) => {
+  const resolveArgs: () => [time: string, message: string] = () => {
+    const [first, second, ...rest] = messageContent.split(' ')
+
+    const lower = second.toLowerCase()
+    const unitCheck = lower.endsWith('s') ? lower.slice(0, -1) : lower
+
+    if (units.has(unitCheck)) {
+      const time = [first, second].join(' ')
+      const content = rest.join(' ')
+
+      return [time, content]
+    }
+
+    const content = [second, ...rest].join(' ')
+    return [first, content]
+  }
+
+  const [time, content] = resolveArgs()
   // TODO
 }
